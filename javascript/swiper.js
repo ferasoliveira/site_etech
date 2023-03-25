b2291ca2b4d772ed1bb64f6e83fdfaab21424d61
guiarthur.as <guilhermearthur.as5@gmail.com>
2023-03-24 23:21:00 -0300

    swiper responsivo

--- javascript/swiper.js
+++ javascript/swiper.js
@@ -16,19 +16,5 @@ var swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
   shadow: true, // Ativa a sombra nos slides
-  on: {
-    slideChangeTransitionStart: function () {
-      // Remove a sombra de todos os slides
-      var slides = document.querySelectorAll(".swiper-slide");
-      slides.forEach(function (slide) {
-        slide.style.boxShadow = "none";
-      });
-
-      // Adiciona a sombra ao slide ativo
-      var activeSlide = document.querySelector(".swiper-slide-active");
-      activeSlide.style.boxShadow = "0 0 10px rgb(0, 199, 255)";
-    },
-  },
 });
